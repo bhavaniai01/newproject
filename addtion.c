ee6812bdff80ad35fddea4399aa5c9f81ee72913
Bhavani R <bhavaniai01@brightskills.com>
2024-01-03 13:30:42 +0530

    added the file

--- addtion.c
+++ addtion.c
@@ -3,8 +3,10 @@
     int main ()
     {
     int a=5;
-    int b=10;
-    printf("add the value a+b");
+    int b=10; 
+    int c;
+     c=a+b;
+    printf("add the value a+b.....%d",c);
     return 0;
 
 }
