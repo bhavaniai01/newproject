259a3b2f73a337d4c19f4ea2db57def6cb9a21e4
Bhavani R <bhavaniai01@brightskills.com>
2024-01-06 12:58:09 +0530

    subtract file

--- subtraction.c
+++ subtraction.c
@@ -1,12 +1,12 @@
 # include<stdio.h>
 
-    int main ()
+    int subtractionprogram ()
     {
     int a=10;
     int b=5; 
     int c;
      c=a-b;
-    printf("add the value a+b.....%d",c);
+    printf("subtract the value a-b.....%d",c);
     return 0;
 
 }
